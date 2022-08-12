d0a10902d61aed0b9ad27bfe05dc701254216246
sb20mad <sb20mad@gmail.com>
2022-08-12 18:48:45 +0300

    add Example002 HellpUser

---
+++ Example001_HelloConsole/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("Hello, World!2");
+Console.WriteLine("Hello, World!2");
+Console.WriteLine("Hello, World!2");
+Console.WriteLine("Hello, World!2");
+Console.WriteLine("Hello, World!2");
