59494727e33195d66f553249fbf8023a97b2dfc6
sb20mad <sb20mad@gmail.com>
2022-08-12 20:02:27 +0300

    add Example 005 If Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользоыетеля");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+Console.WriteLine("Привет");
+Console.WriteLine(username);
+}
